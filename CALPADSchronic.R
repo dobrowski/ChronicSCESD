447bb7fcbb2c83d3b92e463f815f38f85ceddba0
David Dobrowski <dobrowski@gmail.com>
2023-08-14 14:28:38 -0700

    Improve graphs

--- CALPADSchronic.R
+++ CALPADSchronic.R
@@ -7,17 +7,24 @@ library(tidyverse)
 library(readxl)
 library(here)
 library(googlesheets4)
+library(MCOE)
 
 
 sheet <- "https://docs.google.com/spreadsheets/d/1E7x2W-bWkZGenZTPVmlyGSl0LQPfrHc2s8ZLILKUOGw/edit#gid=0"
 
 
-
+### Load files -------
 
 ausd.calpads <- read_xlsx(here("data", "alisal" , "14.2_StudentAbsencesStudentList.xlsx"))
 ausd.calpads.demo <- read_xlsx(here("data", "alisal" , "8.1_StudentProfileList(EOY3).xlsx"))
 
 
+scesd.calpads <- read_csv(here("data", "scesd" , "14.2_StudentAbsencesStudentList.csv"))
+scesd.calpads.demo <- read_csv(here("data", "scesd" , "8.1_StudentProfileList(EOY3).csv"))
+
+
+
+### Functions ----
 
 calpads.join <- function(df, df.demo) {
     
@@ -83,3 +90,165 @@ chronic.group.rate(ausd.calpads.joint, StudentswithDisabilities)
 chronic.group.rate(ausd.calpads.joint, EnglishLearner)
 chronic.group.rate(ausd.calpads.joint, SocioEconomicallyDisadvantaged)
 
+
+
+scesd.calpads.joint <- calpads.join(scesd.calpads, scesd.calpads.demo)
+
+chronic.group.rate(scesd.calpads.joint, EthnicityRace)
+chronic.group.rate(scesd.calpads.joint, Homeless)
+chronic.group.rate(scesd.calpads.joint, StudentswithDisabilities)
+chronic.group.rate(scesd.calpads.joint, EnglishLearner)
+chronic.group.rate(scesd.calpads.joint, SocioEconomicallyDisadvantaged)
+
+
+
+
+
+
+
+
+
+
+# This file is to generate a bar graph to display distance from standard for groups. 
+
+
+
+working <- read_sheet(ss = sheet,
+                      sheet = "Chronic Group") %>%
+    filter(StudentGroup != "N",
+           StudentGroup != "Missing",
+           NumberStudents >= 30) %>%
+    mutate(Group = case_match(StudentGroupCategory,
+                              "Homeless" ~ "Homeless",
+                              "StudentswithDisabilities" ~ "Students with \nDisabilities",
+                              "SocioEconomicallyDisadvantaged" ~ "Socio-Economically \nDisadvantaged",
+                              "EnglishLearner" ~ "English Learner",
+                              .default = StudentGroup
+    ))
+
+
+chronic.dash.graph <- function(dist, dist.name ) {
+    
+    
+    working %>%
+        filter(District == dist) %>%
+ #       mutate(DFS = as.numeric(DFS)) %>%
+        ggplot(aes(x = fct_reorder(Group,PercentChronicAbsent), y = PercentChronicAbsent, )) +
+        geom_col(aes(fill = EstimatedColor, 
+                     color = "black")) +
+        mcoe_theme +
+        scale_fill_identity() +
+        scale_color_identity() +
+        labs(y = "Percent Chronically Absent",
+             title = paste0(dist.name," - Chronically Absent Student Group Results 2023"))
+    
+    
+ #   ggsave(here("output",paste0(dist.name, " - Chronically Absent Student Group Results 2023 ", Sys.Date(),".png")), width = 8, height = 5)    
+    
+}
+
+
+chronic.dash.graph(dist = "ausd.calpads.joint",
+          dist.name = "Alisal")
+
+
+chronic.dash.graph(dist = "scesd.calpads.joint",
+                   dist.name = "Salinas City")
+
+# Haven't updated below here yet ###
+
+
+
+### Comparison to prior year ----
+
+
+con <- mcoe_sql_con()
+
+
+dash <- tbl(con,"DASH_ALL_2022") %>%
+    filter(countyname == "Monterey",
+           rtype == "D",
+           indicator == "chronic" 
+           ) %>%
+    collect()  %>%
+    mutate(Group = case_match(studentgroup,
+                              "HOM" ~ "Homeless",
+                              "SWD" ~ "Students with \nDisabilities",
+                              "SED" ~ "Socio-Economically \nDisadvantaged",
+                              "HI" ~ "Hispanic",
+                              "EL" ~ "English Learner",
+                              "AS" ~ "Asian",
+                              "FI" ~ "Filipino",
+                              "WH" ~ "White",
+                              "MR" ~ "Multiple",
+                              "AA" ~ "Black/African Am",
+                              "AI" ~ "Am Indian/Alskn Nat",
+                              .default = studentgroup
+    ))
+
+
+chronic.dash.comp <- function(dist, assessment = "ELA", dist.name ) {
+    
+    
+    work.group <-   working %>%
+        filter(District == dist
+               ) %>%
+        select(Group) %>%
+        unique() %>%
+        flatten()
+    
+    dash2 <- dash %>%
+        filter(str_detect(districtname, dist.name),
+               Group %in% work.group
+        ) %>%
+        select(districtname, indicator, currstatus, Group) %>%
+        mutate(EstimatedColor = "Light Gray") %>%
+        rename(PercentChronicAbsent = currstatus)
+    
+    
+    
+df <-    working %>%
+        filter(District == dist
+               ) %>%
+        mutate(PercentChronicAbsent = as.numeric(PercentChronicAbsent)) %>%
+        bind_rows(dash2) %>%
+        mutate(EstimatedColor = as_factor(EstimatedColor)) 
+        
+# Sorts only by the current year
+ leveler <- df %>% 
+     filter(EstimatedColor != "Light Gray") %>%
+     arrange(PercentChronicAbsent) 
+
+ levelss <- leveler$Group %>% union(working$Group %>% unique())
+
+
+df %>%   
+    mutate(Group = factor(Group, levels = levelss), # Sorts only by the current year
+           EstimatedColor = fct_relevel(EstimatedColor,"Light Gray" ). # Puts gray to the left of color
+           ) %>%
+    ggplot(aes(x = Group, y = PercentChronicAbsent)) +
+#    ggplot(aes(x = fct_reorder(Group,PercentChronicAbsent), y = PercentChronicAbsent)) +
+        geom_col(aes(fill = EstimatedColor,
+                     color = "black"),
+                 position = "dodge2") +
+        mcoe_theme +
+        scale_fill_identity() +
+        scale_color_identity() +
+        labs(y = "Percent Chronically Absent",
+             title = paste0(dist.name," - Chronically Absent Student Group Results 2023"),
+             subtitle = "Gray is 2022 results and Colored bars are 2023 with the estimated Dashboard color")
+    
+    
+#    ggsave(here("output",paste0(dist.name, " - ",assessment," CAASPP Student Group Results 2022 and 2023 Comparison ", Sys.Date(),".png")), width = 8, height = 5)    
+    
+}
+
+
+
+chronic.dash.comp(dist = "ausd.calpads.joint",
+         dist.name = "Alisal")
+
+
+
+chronic.dash.comp(dist = "scesd.calpads.joint",
+                   dist.name = "Salinas City")
