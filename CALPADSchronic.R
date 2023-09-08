a9b7fb47aac7138ce0538a26edffa13c1e04946b
David Dobrowski <dobrowski@gmail.com>
2023-09-08 10:56:13 -0700

    Year end CALPADS calculations

--- CALPADSchronic.R
+++ CALPADSchronic.R
@@ -18,10 +18,25 @@ sheet <- "https://docs.google.com/spreadsheets/d/1E7x2W-bWkZGenZTPVmlyGSl0LQPfrH
 ausd.calpads <- read_xlsx(here("data", "alisal" , "14.2_StudentAbsencesStudentList.xlsx"))
 ausd.calpads.demo <- read_xlsx(here("data", "alisal" , "8.1_StudentProfileList(EOY3).xlsx"))
 
-
 scesd.calpads <- read_csv(here("data", "scesd" , "14.2_StudentAbsencesStudentList.csv"))
 scesd.calpads.demo <- read_csv(here("data", "scesd" , "8.1_StudentProfileList(EOY3).csv"))
 
+nmcusd.calpads <- read_csv(here("data", "nmcusd" , "14.2_StudentAbsencesStudentList.csv"))
+nmcusd.calpads.demo <- read_csv(here("data", "nmcusd" , "8.1_StudentProfileList(EOY3).csv"))
+
+soledad.calpads <- read_csv(here("data", "soledad" , "14.2_StudentAbsencesStudentList.csv"))
+soledad.calpads.demo <- read_csv(here("data", "soledad" , "8.1_StudentProfileList(EOY3).csv"))
+
+spreckels.calpads <- read_xlsx(here("data", "spreckels" , "14.2_StudentAbsencesStudentList.xlsx"),
+                               range = "G9:AE950") %>%
+    janitor::clean_names("upper_camel") %>%
+    rename(SSID = Ssid) %>%
+    mutate(AdaGeneratingIndependentStudyDays = as.numeric(AdaGeneratingIndependentStudyDays))
+spreckels.calpads.demo <- read_xlsx(here("data", "spreckels" , "8.1_StudentProfileList(EOY3).xlsx"),
+                                    range = "G10:AT968")%>%
+    janitor::clean_names("upper_camel") %>%
+    rename(SSID = Ssid) %>%
+    mutate(StudentswithDisabilities = StudentsWithDisabilities)
 
 
 ### Functions ----
@@ -30,6 +45,9 @@ calpads.join <- function(df, df.demo) {
     
     
     df.calpads2 <- df %>%
+        filter(DaysExpectedA >= 1,
+               Grade %in% c("KN",1,2,3,4,5,6,7,8,  "01","02","03","04","05","06","07","08"   )
+               ) %>%
         group_by(SSID, StudentName, Gender, Grade ) %>% #, Ethnicity, EnglishLearner, SocioEconomicallyDisadvantaged) %>%
         summarise(across(.cols =   c(DaysExpectedA:DaysAbsentCEFG),
                          ~ sum(.x, na.rm = TRUE)
@@ -102,16 +120,47 @@ chronic.group.rate(scesd.calpads.joint, SocioEconomicallyDisadvantaged)
 
 
 
+nmcusd.calpads.joint <- calpads.join(nmcusd.calpads, nmcusd.calpads.demo)
+
+chronic.group.rate(nmcusd.calpads.joint, EthnicityRace)
+chronic.group.rate(nmcusd.calpads.joint, Homeless)
+chronic.group.rate(nmcusd.calpads.joint, StudentswithDisabilities)
+chronic.group.rate(nmcusd.calpads.joint, EnglishLearner)
+chronic.group.rate(nmcusd.calpads.joint, SocioEconomicallyDisadvantaged)
 
 
+nmcusd.calpads.joint %>%
+    mutate(All = "Y") %>%
+    chronic.group.rate(All)
 
 
+temp <- nmcusd.calpads %>%
+    mutate(AbsenceRate2 = 100*DaysAbsentCEFG/DaysExpectedA,
+           chronic = if_else(AbsenceRate2 >= 10, TRUE, FALSE),
+           dupes = duplicated(SSID))
+
+mean(temp$chronic, na.rm = TRUE)
 
 
+soledad.calpads.joint <- calpads.join(soledad.calpads, soledad.calpads.demo)
 
-# This file is to generate a bar graph to display distance from standard for groups. 
+chronic.group.rate(soledad.calpads.joint, EthnicityRace)
+chronic.group.rate(soledad.calpads.joint, Homeless)
+chronic.group.rate(soledad.calpads.joint, StudentswithDisabilities)
+chronic.group.rate(soledad.calpads.joint, EnglishLearner)
+chronic.group.rate(soledad.calpads.joint, SocioEconomicallyDisadvantaged)
 
 
+spreckels.calpads.joint <- calpads.join(spreckels.calpads, spreckels.calpads.demo)
+
+chronic.group.rate(spreckels.calpads.joint, EthnicityRace)
+chronic.group.rate(spreckels.calpads.joint, Homeless)
+chronic.group.rate(spreckels.calpads.joint, StudentswithDisabilities)
+chronic.group.rate(spreckels.calpads.joint, EnglishLearner)
+chronic.group.rate(spreckels.calpads.joint, SocioEconomicallyDisadvantaged)
+
+
+### Graphing Single Year -----
 
 working <- read_sheet(ss = sheet,
                       sheet = "Chronic Group") %>%
@@ -143,7 +192,7 @@ chronic.dash.graph <- function(dist, dist.name ) {
              title = paste0(dist.name," - Chronically Absent Student Group Results 2023"))
     
     
- #   ggsave(here("output",paste0(dist.name, " - Chronically Absent Student Group Results 2023 ", Sys.Date(),".png")), width = 8, height = 5)    
+    ggsave(here("output",paste0(dist.name, " - Chronically Absent Student Group Results 2023 ", Sys.Date(),".png")), width = 8, height = 5)    
     
 }
 
@@ -151,12 +200,18 @@ chronic.dash.graph <- function(dist, dist.name ) {
 chronic.dash.graph(dist = "ausd.calpads.joint",
           dist.name = "Alisal")
 
-
 chronic.dash.graph(dist = "scesd.calpads.joint",
                    dist.name = "Salinas City")
 
-# Haven't updated below here yet ###
+chronic.dash.graph(dist = "nmcusd.calpads.joint",
+                   dist.name = "North Monterey County")
 
+chronic.dash.graph(dist = "soledad.calpads.joint",
+                   dist.name = "Soledad")
+
+
+chronic.dash.graph(dist = "spreckels.calpads.joint",
+                   dist.name = "Spreckels")
 
 
 ### Comparison to prior year ----
@@ -224,7 +279,7 @@ df <-    working %>%
 
 df %>%   
     mutate(Group = factor(Group, levels = levelss), # Sorts only by the current year
-           EstimatedColor = fct_relevel(EstimatedColor,"Light Gray" ). # Puts gray to the left of color
+           EstimatedColor = fct_relevel(EstimatedColor,"Light Gray" ) # Puts gray to the left of color
            ) %>%
     ggplot(aes(x = Group, y = PercentChronicAbsent)) +
 #    ggplot(aes(x = fct_reorder(Group,PercentChronicAbsent), y = PercentChronicAbsent)) +
@@ -232,6 +287,7 @@ df %>%
                      color = "black"),
                  position = "dodge2") +
         mcoe_theme +
+    {if(length(unique(df$Group)) >=8 )scale_x_discrete(guide = guide_axis(n.dodge = 2))} + #Fixes the overlapping axis labels to make them alternate if lots of columns
         scale_fill_identity() +
         scale_color_identity() +
         labs(y = "Percent Chronically Absent",
@@ -239,7 +295,7 @@ df %>%
              subtitle = "Gray is 2022 results and Colored bars are 2023 with the estimated Dashboard color")
     
     
-#    ggsave(here("output",paste0(dist.name, " - ",assessment," CAASPP Student Group Results 2022 and 2023 Comparison ", Sys.Date(),".png")), width = 8, height = 5)    
+    ggsave(here("output",paste0(dist.name," - Chronically Absent Student Group Results 2022 and 2023 Comparison ", Sys.Date(),".png")), width = 8, height = 5)    
     
 }
 
@@ -248,7 +304,15 @@ df %>%
 chronic.dash.comp(dist = "ausd.calpads.joint",
          dist.name = "Alisal")
 
-
-
 chronic.dash.comp(dist = "scesd.calpads.joint",
                    dist.name = "Salinas City")
+
+chronic.dash.comp(dist = "nmcusd.calpads.joint",
+                  dist.name = "North Monterey County")
+
+chronic.dash.comp(dist = "soledad.calpads.joint",
+                   dist.name = "Soledad")
+
+
+chronic.dash.comp(dist = "spreckels.calpads.joint",
+    dist.name = "Spreckels")
